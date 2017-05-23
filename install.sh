4c64c8b8b05cd03824e77d591923aec1775c7f7b
gtt142 <garif-tim@yandex.ru>
2017-05-23 23:10:43 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,21 @@
 #!/bin/sh
-sudo -i << END
-apt-get update
-apt-get -y install apache2
-apt-get -y install nginx
-apt-get -y install sysstat
-apt-get -y install bc
-a2enmod cgi
-cp index.cgi /var/www/html/index.cgi
-chmod 755 /var/www/html/index.cgi
-cp apache2-sysinfo.conf /etc/apache2/sites-available/apache2-sysinfo.conf
-rm /etc/nginx/sites-enabled/default
-cp nginx-sysinfo.conf /etc/nginx/sites-enabled/default
-rm /etc/apache2/ports.conf
-cp ports.conf /etc/apache2/ports.conf 
-a2ensite apache2-sysinfo
-service apache2 reload
-service apache2 restart
-touch /var/log/mpstat.log  /var/log/iostat.log  /var/log/tcp.log /var/log/udp.log /var/log/df.log /var/log/network.log
-crontab cron.bak
-service nginx restart
-END
+sudo apt-get update
+sudo apt-get -y install apache2
+sudo apt-get -y install nginx
+sudo apt-get -y install sysstat
+sudo apt-get -y install bc
+sudo a2enmod cgi
+sudo mkdir /var/www/html/sysinfo
+sudo cp index.cgi /var/www/html/sysinfo/index.cgi
+sudo chmod 755 /var/www/html/sysinfo/index.cgi
+sudo cp apache2-sysinfo.conf /etc/apache2/sites-available/apache2-sysinfo.conf
+sudo rm /etc/nginx/sites-enabled/default
+sudo cp nginx-sysinfo.conf /etc/nginx/sites-enabled/default
+sudo rm /etc/apache2/ports.conf
+sudo cp ports.conf /etc/apache2/ports.conf 
+sudo a2ensite apache2-sysinfo
+sudo service apache2 reload
+sudo service apache2 restart
+sudo touch /var/log/mpstat.log  /var/log/iostat.log  /var/log/tcp.log /var/log/udp.log /var/log/df.log /var/log/network.log
+sudo crontab cron.bak
+sudo service nginx restart
